6bbbc225a691aa7100711c0440d4df7db58da57e
Lionking16 <pavansingaraju@outlook.com>
2017-07-23 22:33:31 +0530

    Removed unnecessary imports from code

--- vipanchi/JSonTryOuts/src/main/java/org/vipanchi/JSonTryOuts/TestJackson.java
+++ vipanchi/JSonTryOuts/src/main/java/org/vipanchi/JSonTryOuts/TestJackson.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
